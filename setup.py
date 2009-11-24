2a7fe67adeb051ef81f5c0c62eb72af21ad982fb
Brian Luft <brian@unbracketed.com>
2009-11-23 18:23:27 -0800

    updates with basic project scaffolding; tipi init works

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "tipi",
+    version = "0.1",
+    url = 'http://unbracketed.com/projects/tipi/',
+    license = 'MIT',
+    description = "Aims to provide conveniences for creating and managing Python virtualenvs",
+    long_description = read('README'),
+    keywords = 'utilities commands virtualenv packages modules installation deployment dependencies',
+    author = 'Brian Luft',
+    author_email = 'brian@unbracketed.com',
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+
+    install_requires = ['virtualenv','virtualenv-commands'],
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', 
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    scripts=['bin/tipi', 'bin/tipi-init',],
+)
+
